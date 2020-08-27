0f93c9b75cbbe3a80e1192d7fcd2d14ae488e24f
lotosniper <70276686+lotosniper@users.noreply.github.com>
2020-08-27 10:24:11 -0300

    Add files via upload

---
+++ csource/static.xx.fbcdn.net/rsrc.php/v3/yN/r/L5Tl9t11rLP1f8e.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["TJqSJ"]); }
+
+__d("DraftEditorFlushControlled",["ReactDOM"],(function(a,b,c,d,e,f){a=b("ReactDOM").unstable_flushControlled;e.exports=a}),null);
---
+++ csource/static.xx.fbcdn.net/rsrc.php/v3/yx/r/aa07suRDSSu1f8e.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["oViJu"]); }
+
+__d("FBJSON",[],(function(a,b,c,d,e,f){e.exports={parse:JSON.parse,stringify:JSON.stringify}}),null);
